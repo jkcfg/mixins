0fe5743310c6263648ee02847ab832fe7984f2e7
Damien Lespiau <damien@weave.works>
2018-12-27 20:22:26 +0100

    example: Add a simple mix example

---
+++ examples/mix-simple/namespace.js
@@ -0,0 +1,42 @@
+import { mix, patch } from '@jkcfg/mixins';
+import std from 'std';
+
+const withNamespace = ns => (
+  obj => patch(obj, { metadata: { namespace: ns } })
+);
+
+const deployment = {
+  apiVersion: 'apps/v1',
+  kind: 'Deployment',
+  metadata: {
+    labels: {
+      app: 'nginx',
+    },
+  },
+  spec: {
+    selector: {
+      matchLabels: {
+        app: 'nginx',
+      },
+    },
+    replicas: 2,
+    template: {
+      metadata: {
+        labels: {
+          app: 'nginx',
+        },
+      },
+      spec: {
+        containers: [{
+          name: 'nginx',
+          image: 'nginx:1.7.9',
+          ports: [{
+            containerPort: 80,
+          }],
+        }],
+      },
+    },
+  },
+};
+
+std.log(mix(deployment, withNamespace('monitoring')), { format: std.Format.YAML });
